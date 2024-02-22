44542097f85f8db70896978177218255a369f864
Alessandro <irelip.a@gmail.com>
2024-02-22 11:58:14 +0100

    v1

--- src/main/java/com/generation/jadventures/model/dto/guild/GuildDtoWQuest.java
+++ src/main/java/com/generation/jadventures/model/dto/guild/GuildDtoWQuest.java
@@ -4,6 +4,15 @@ import java.util.List;
 
 import com.generation.jadventures.model.entities.Quest;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class GuildDtoWQuest extends GuildDtoBase {
 
     private Integer id;
