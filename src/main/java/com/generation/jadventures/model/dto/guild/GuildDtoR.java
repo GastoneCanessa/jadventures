8f009a7e30e7c3b51f39070cc6eee070f3db4581
Chiara Belfiore <Chiara@example.com>
2024-02-22 11:07:48 +0100

    chiaradto1

---
+++ src/main/java/com/generation/jadventures/model/dto/guild/GuildDtoR.java
@@ -0,0 +1,6 @@
+package com.generation.jadventures.model.dto.guild;
+
+public class GuildDtoR extends GuildDtoBase {
+
+    private Integer id; // non sono sicura che serva
+}
---
+++ src/main/java/com/generation/jadventures/model/dto/guild/GuildDtoWQuest.java
@@ -0,0 +1,11 @@
+package com.generation.jadventures.model.dto.guild;
+
+import java.util.List;
+
+import com.generation.jadventures.model.entities.Quest;
+
+public class GuildDtoWQuest extends GuildDtoBase {
+
+    private Integer id;
+    private List<Quest> posted_quests;
+}
