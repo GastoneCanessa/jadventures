77b6d140be3808069d9807008758ba8e189e0fd7
Alessandro <irelip.a@gmail.com>
2024-02-22 16:06:00 +0100

    v2

--- src/main/java/com/generation/jadventures/model/entities/Adventurer.java
+++ src/main/java/com/generation/jadventures/model/entities/Adventurer.java
@@ -1,18 +1,21 @@
 package com.generation.jadventures.model.entities;
 
+import java.time.LocalDate;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @Entity
@@ -25,8 +28,13 @@ public class Adventurer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    private String name, surname, adventurer_rank, role;
+    private LocalDate dob;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "passenger",cascade = CascadeType.REMOVE,fetch = FetchType.EAGER)
-    private List<Ticket> tickets;
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "party_id")
+    private Party party;
+
 }
